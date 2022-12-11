a08462c9dc21b14596a732639742f3217135aa94
snehal honamane <smhonamane31@gmail.com>
2022-12-11 23:39:42 +0530

    01_swap_assignment
    01_swap_assignment

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,23 @@
+var sweety="Sweety"; //initialization
+var cutie="Cutie";
+var swap; //declaration
+console.log("------Before Swapping------\n Sweety\n Cutie\n");
+swap=sweety;
+sweety=cutie;
+cutie=swap;
+console.log("------After Swapping------");
+console.log(sweety);
+console.log(cutie,"\n");
+
+
+var num1=100, num2=200, num3=300;
+console.log("------Before Swapping------\n 100\n 200\n 300\n");
+swap=num1;
+num1=num2;
+num2=num3;
+num3=swap;
+console.log("------After Swapping------");
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
