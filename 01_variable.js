f9e0c89760c6568bf8853a0f43f1cce74502a39f
snehal honamane <smhonamane31@gmail.com>
2022-12-11 23:43:47 +0530

    01_variable
    01_variable

---
+++ 01_variable.js
@@ -0,0 +1,21 @@
+ var myname;   //Variable Declaration
+ myname="Snehal Honamane";  //Variable initialization
+ console.log(myname);
+ 
+ var myname="Sachin Tndulkar";  //Variable Declaration and initialization at same time
+ console.log(myname);
+
+ myname="MS Dhoni";       //Variable update
+ console.log(myname);
+
+  var companyName="";
+  console.log(companyName);
+  console.log(typeof companyName);
+
+  console.log(Text45); var weight=45;
+  console.log(typeof"Billgates"); var microsoftOwner="Billgates";
+  console.log(typeof false);var areYouMad=false;
+
+  String microsoftOwner="Billgates";  //java
+  var microsoftOwner="Billgates";
+  microsoftOwner=true;
