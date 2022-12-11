f8032e75cbe78e0e16405c236ba13c5747bffcfe
snehal honamane <smhonamane31@gmail.com>
2022-12-11 23:40:30 +0530

    01_variable_assignment
    01_variable_assignment

---
+++ 01-variable-assign.js
@@ -0,0 +1,17 @@
+var myName;
+myName="Snehal Honamane";
+console.log(myName);
+
+myFullName="Snehal Mailarling Honamane";
+console.log(myFullName);
+
+var myLove="I LOVE JAVASCRIPT";
+console.log(myLove);
+
+var myName="Snehal Honamane";
+var age="24";
+var address="magalwedha";
+var pin_code="413305";
+var gender="Female";
+var is_married="yes";
+console.log(myName,age,address,pin_code,gender,is_married);
