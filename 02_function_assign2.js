325421188110859ee33185dd43bd8c9208826e38
snehal honamane <smhonamane31@gmail.com>
2022-12-11 23:38:07 +0530

    02_function_assignment
    02_function_assignment

---
+++ 02_function_assign2.js
@@ -0,0 +1,9 @@
+function bankDetails(bankName, accountNum, location, pinCode,)
+{
+    console.log("Bank Details:",bankName, accountNum, location, pinCode);
+    
+    
+}
+bankDetails("CITI", 3456782345, 'PUNE', 431002);
+bankDetails("Axsis",7856782345, 'Mumbai', 441202);
+bankDetails("HDFC", 8956782345, 'Pune', 631202);
